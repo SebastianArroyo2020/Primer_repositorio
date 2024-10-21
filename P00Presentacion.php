b974307bc3e504d77d83baf998abb1b2633b9736
SebastianArroyo2020 <sebastian.arroyo.castro2008@gmail.com>
2024-10-21 09:30:10 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,17 @@
+<?php
+/* CBTIS 89
+Programa que suma dos numeros enteros
+Sebastian Arroyo Castro
+3°A Programacion Matutino 
+*/
+
+echo "Arroyo Castro Sebastian","<br>";
+echo "16 años","<br>";
+echo "Programacion 3°A T.M.","<br>";
+echo "CBTIS 89","<br>";
+echo "Mexico","<br>";
+echo "Durango,DGO","<br>";
+echo "Canatlan 401","<br>";
+
+
+?>
