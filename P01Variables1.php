6b79f01dd92438b398595930121b15db0518acbb
SebastianArroyo2020 <sebastian.arroyo.castro2008@gmail.com>
2024-10-21 09:10:41 -0600

    Add files via upload

---
+++ P01Variables1.php
@@ -0,0 +1,20 @@
+
+<?php 
+$dia = 24; //Se declara una variable tipo integer.
+$sueldo = 758.43; //Se decalara una variable tipo double
+$nombre = "juan"; //Se decalara una variable de tipo string
+$exite = true; //Se decalara una variable tipo boolean
+
+echo "Variable entera:";
+echo $dia;echo "<br>";
+echo "Variable double:";
+echo $sueldo;echo "<br>";
+echo "Variable string:";
+echo $nombre; echo "<br>";
+echo "Variable boolean:";
+echo $exite;
+echo "<br>";
+$cadena1= "Hola";
+$cadena2= "Mundo";
+echo $cadena1. " ".$cadena2;
+?>
